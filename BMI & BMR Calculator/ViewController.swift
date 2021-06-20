3b47bdd3260b44862bc0c4e886354c57b21ddbb2
Eagleclw <kartalemreburuk@gmail.com>
2021-06-20 15:17:58 +0300

    Initial Commit

---
+++ BMI & BMR Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI & BMR Calculator
+//
+//  Created by Kartal Emre Buruk on 20.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
